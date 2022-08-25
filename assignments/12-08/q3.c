ee307b33d83d57243cf0b94e9e81d2729f373360
ayushanand18 <kingwarrior880@gmail.com>
2022-08-25 17:14:25 +0530

    added assignment

--- assignments/12-08/q3.c
+++ assignments/12-08/q3.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
+    int n, sum;
+    printf("enter size of the array:\t");
+    scanf("%d", &n);
+    int *p = malloc(n*sizeof(int));
+    printf("enter %d elements one by one:\t", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", p+i);
+        sum += *(p+i);
+    }
     
+    // print the array
+    printf("the array inputted is:\t");
+    for(int i=0; i<n; i++){
+        printf("%d ", *(p+i));
+    }
+    printf("\nsum is %d\n", sum);
 }
